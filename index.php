18abca6a0e31a171dc3ce626e10f0b7d5604e1cf
remiPra <remipradere@gmail.com>
2020-06-08 09:38:04 +0200

    modif de l'index.php

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+require 'controllers/controllerRouter.php';
+
+
+if ($_SESSION == false and isset($_GET['action'])) {
+    ////////////////////////////////////////////////////
+    ///////////Partie Frond-end///////////////////////
+    //////////////////////////////////////////////////
+    // redirection page d'accueil
+
+    switch (($_GET['action'])) {
+        
+        case 'home':
+            index();
+            break;
+        case 'cart':
+        case 'connexion':
+            connexion();
+            break;
+        case 'blog':
+            blog();
+            break;
+        case 'signIn':
+            signIn();
+            break;
+        case 'checkPassword':
+            checkPassword();
+            break;
+        default:
+            index();
+            break;
+    }
+} else if (isset($_SESSION) and isset($_GET['action'])) {
+    switch (($_GET['action'])) {
+        case 'home':
+            index();
+            break;
+        case 'cart':
+            cart();
+            break;
+        case 'stepOrder':
+            stepOrder();
+            break;
+        case 'stepOrderAxios':
+            echo "<script type='text/javascript'>document.location.replace('proceed.php?action=prepareOrder&value=".$_SESSION['name']."');</script>";
+            break;
+        case 'deconnexion':
+            deconnexion();
+            break;
+        default:
+            index();
+            break;
+    }
+} else {
+    index();
+}
